45b5175bdbfe0f77fddb777422488294efc052a8
Harsh Singh <146654269+harshsinghh11@users.noreply.github.com>
2024-07-30 21:32:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Your message has been sent!');
+        form.reset();
+    });
+});
